b6734246975e58cdef5cf067ff831738c0773479
huyquang6597 <quanghuy6597@gmail.com>
2022-05-20 14:26:45 +0700

    DictionaryServlet

---
+++ SQL/thuvien/src/main/java/DictionaryServlet.java
@@ -0,0 +1,36 @@
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
+
+@WebServlet(name = "TranslationServlet", value = "/translate")
+public class DictionaryServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Map<String, String> dictionary = new HashMap<>();
+        dictionary.put("hello", "xin chào");
+        dictionary.put("how", "Thế nào");
+        dictionary.put("book", "Quyển vở");
+        dictionary.put("computer", "Máy tính");
+
+        String search = request.getParameter("txtSearch");
+        PrintWriter printWriter = response.getWriter();
+        printWriter.println("<html>");
+        String result = dictionary.get(search);
+        if (result != null) {
+            printWriter.println("Word: " + search);
+            printWriter.println("Result: " + result);
+        } else {
+            printWriter.println("Not found");
+        }
+        printWriter.println("</html>");
+    }
+}
