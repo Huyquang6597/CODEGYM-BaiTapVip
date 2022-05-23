ce225633a72bfb9413bc0c433c12a85e2094da8d
huyquang6597 <quanghuy6597@gmail.com>
2022-05-23 15:34:44 +0700

    maytinhjsp

---
+++ SQL/maytinh/src/main/java/servlet/CalculatorServlet.java
@@ -0,0 +1,36 @@
+package servlet;
+
+import model.Calculator;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
+@WebServlet(name = "CalculatorServlet", value = "/calculate")
+public class CalculatorServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        float firstOperand = Integer.parseInt(request.getParameter("first-operand"));
+        float secondOperand = Integer.parseInt(request.getParameter("second-operand"));
+        char operator = request.getParameter("operator").charAt(0);
+        PrintWriter printWriter = response.getWriter();
+        printWriter.println("<html>");
+        printWriter.println("<h1>Result: </h1>");
+        try {
+            float result = Calculator.calculate(firstOperand, secondOperand, operator);
+            printWriter.println(firstOperand + " " + operator + " " + secondOperand + "=" + result);
+        }catch (Exception e){
+            printWriter.println("Error");
+        }
+    printWriter.println("</html>");
+    }
+
+}
