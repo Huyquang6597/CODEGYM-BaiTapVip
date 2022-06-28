f0543888865ad090a0cefeabf25dd222b4ea8862
huyquang6597 <quanghuy6597@gmail.com>
2022-06-28 08:29:41 +0700

    done-reset

---
+++ SpringMVC/springandajax/src/main/java/com/example/springandajax/SpringandajaxApplication.java
@@ -0,0 +1,13 @@
+package com.example.springandajax;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringandajaxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringandajaxApplication.class, args);
+    }
+
+}
---
+++ SpringMVC/springandajax/src/test/java/com/example/springandajax/SpringandajaxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springandajax;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringandajaxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
